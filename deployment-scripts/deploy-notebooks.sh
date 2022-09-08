0cae29570a0ffed91a3375a3eeafdece1c4cce8e
SGannavarapu <111972516+SGannavarapu@users.noreply.github.com>
2022-09-08 17:06:26 +0530

    Update deploy-notebooks.sh

--- deployment-scripts/deploy-notebooks.sh
+++ deployment-scripts/deploy-notebooks.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # Must be run in the directory with the notebooks (spaces in names in Bash can cause issues)
-tenant_id=$1
-client_id=$2
-client_secret=$3
-subscription_id=$4
-resourceGroup=$5
-workspaceName=$6
-notebookPathUnderWorkspace=$7
+# tenant_id=$1
+# client_id=$2
+# client_secret=$3
+# subscription_id=$4
+# resourceGroup=$5
+# workspaceName=$6
+# notebookPathUnderWorkspace=$7
 
-echo "tenant_id : $tenant_id"
-echo "client_id : $client_id"
-echo "client_secret : $client_secret"
-echo "subscription_id : $subscription_id"
-echo "resourceGroup : $resourceGroup"
-echo "workspaceName : $workspaceName"
-echo "notebookPathUnderWorkspace : $notebookPathUnderWorkspace"
+# echo "tenant_id : $tenant_id"
+# echo "client_id : $client_id"
+# echo "client_secret : $client_secret"
+# echo "subscription_id : $subscription_id"
+# echo "resourceGroup : $resourceGroup"
+# echo "workspaceName : $workspaceName"
+# echo "notebookPathUnderWorkspace : $notebookPathUnderWorkspace"
 
-# tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47"
-# client_id="e5fc5f4d-f803-44c4-8f2a-92c9a920d226"
-# client_secret="-Gc8Q~au_f3lVzosZMDW9u3zcocbXKcbaV_RZcKw"
-# subscription_id="011d72ba-8ffe-4358-a191-e5d110d97a2d"
-# resourceGroup="Demo-ADB"
-# workspaceName="gsdemoadb"
-# notebookPathUnderWorkspace="/MyProject"
+tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47"
+client_id="e5fc5f4d-f803-44c4-8f2a-92c9a920d226"
+client_secret="-Gc8Q~au_f3lVzosZMDW9u3zcocbXKcbaV_RZcKw"
+subscription_id="011d72ba-8ffe-4358-a191-e5d110d97a2d"
+resourceGroup="Demo-ADB"
+workspaceName="gsdemoadb"
+notebookPathUnderWorkspace="/MyProject"
 
 azure_databricks_resource_id="2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
 resourceId="/subscriptions/$subscription_id/resourceGroups/$resourceGroup/providers/Microsoft.Databricks/workspaces/$workspaceName"
