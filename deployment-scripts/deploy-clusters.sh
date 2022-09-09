9121647d39bcc88a694a7312b1761e8f946ed8f9
SGannavarapu <111972516+SGannavarapu@users.noreply.github.com>
2022-09-09 16:48:40 +0530

    Update deploy-clusters.sh

--- deployment-scripts/deploy-clusters.sh
+++ deployment-scripts/deploy-clusters.sh
@@ -10,52 +10,11 @@ client_secret=$3
 subscription_id=$4
 resourceGroup=$5
 workspaceName=$6
-
-# tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47"
-# client_id="e5fc5f4d-f803-44c4-8f2a-92c9a920d226"
-# client_secret="-Gc8Q~au_f3lVzosZMDW9u3zcocbXKcbaV_RZcKw"
-# subscription_id="011d72ba-8ffe-4358-a191-e5d110d97a2d"
-# resourceGroup="Demo-ADB"
-# workspaceName="gsdemoadb"
-# notebookPathUnderWorkspace="/MyProject"
-
+accessToken=$7
+workspaceUrl=$8
 
 azure_databricks_resource_id="2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
 resourceId="/subscriptions/$subscription_id/resourceGroups/$resourceGroup/providers/Microsoft.Databricks/workspaces/$workspaceName"
-
-######################################################################################
-# Get access tokens for Databricks API
-######################################################################################
-
-# accessToken=$(curl -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
-#   -F resource=$azure_databricks_resource_id \
-#   -F client_id=$client_id \
-#   -F grant_type=client_credentials \
-#   -F client_secret=$client_secret | jq .access_token --raw-output) 
-
-# managementToken=$(curl -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
-#   -F resource=https://management.core.windows.net/ \
-#   -F client_id=$client_id \
-#   -F grant_type=client_credentials \
-#   -F client_secret=$client_secret | jq .access_token --raw-output) 
-
-# accessToken="dapi81b00e319c187e4d0a4a973e13d43cab-2"
-accessToken=$7
-
-
-######################################################################################
-# Get Databricks workspace URL (e.g. adb-5946405904802522.2.azuredatabricks.net)
-######################################################################################
-# workspaceUrl=$(curl -X GET \
-#         -H "Content-Type: application/json" \
-#         -H "Authorization: Bearer $managementToken" \
-#         https://management.azure.com/subscriptions/$subscription_id/resourcegroups/$resourceGroup/providers/Microsoft.Databricks/workspaces/$workspaceName?api-version=2018-04-01 \
-#         | jq .properties.workspaceUrl --raw-output)
-
-# workspaceUrl="adb-3185198617161307.7.azuredatabricks.net"
-workspaceUrl=$8
-echo "Databricks workspaceUrl: $workspaceUrl"
-
 
 ######################################################################################
 # Deploy clusters (Add or Update existing)
