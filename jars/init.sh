af0f1e7b38796b8dad9a8f7e0af214fa41afa8be
SG250678 <SG250678@ncr.com>
2022-11-13 17:17:09 +0530

    rerun

--- jars/init.sh
+++ jars/init.sh
@@ -1,1 +1,15 @@
-cp `/dbfs/FileStore/jars/*.jar /databricks/jars/
+%sh
+#!/bin/bash
+rm -rf /databricks/jars/ordersystem.jar
+cp /dbfs/<path_to_replacement_jar>/<replacement_jar_filename>.jar /databricks/jars/
+
+rm -rf /databricks/jars/crmsystem.jar
+cp /dbfs/<path_to_replacement_jar>/<replacement_jar_filename>.jar /databricks/jars/
+
+echo library installed
+# cp /dbfs/FileStore/jars/*.jar /databricks/jars/
+# %sh
+
+# #!/bin/bash
+# rm -rf /databricks/jars/<jar_filename_to_remove>.jar
+# cp /dbfs/<path_to_replacement_jar>/<replacement_jar_filename>.jar /databricks/jars/
